a3e8a7021ad6da905dc3f15667a28f7af7e8955c
Yongwook <kyw9885@gmail.com>
2024-11-11 14:44:26 +0900

    [1111] AOP

--- 01_lecture-source/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/aop/Application.java
+++ 01_lecture-source/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/aop/Application.java
@@ -9,6 +9,15 @@ public class Application {
         ApplicationContext context
                 = new AnnotationConfigApplicationContext(ContextConfig.class);
 
-        
+        MemberService memberService
+                = context.getBean("memberService", MemberService.class);
+
+        System.out.println("======selectMembers======");
+        System.out.println(memberService.selectMembers());
+        System.out.println("======selectMember======");
+        System.out.println(memberService.selectMember(1L));
+
+
     }
+
 }
