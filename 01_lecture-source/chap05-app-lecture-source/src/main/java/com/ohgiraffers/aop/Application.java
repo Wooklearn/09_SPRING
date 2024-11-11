f5f49dcaaa9f492738055b322f58071b26431727
Yongwook <kyw9885@gmail.com>
2024-11-11 10:19:51 +0900

    [1111] aop

---
+++ 01_lecture-source/chap05-app-lecture-source/src/main/java/com/ohgiraffers/aop/Application.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext(ContextConfig.class);
+
+        
+    }
+}
---
+++ 01_lecture-source/chap05-app-lecture-source/src/main/java/com/ohgiraffers/aop/ContextConfig.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.aop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ohgiraffers.aop")
+public class ContextConfig {
+}
